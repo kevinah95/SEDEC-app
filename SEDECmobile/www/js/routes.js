54f3f5b4c82204782f944f260dfea1c25a944574
Kevin Hernandez Rostran <kevinah95@gmail.com>
2017-06-01 21:23:38 -0600

    Integration

--- SEDECmobile/www/js/routes.js
+++ SEDECmobile/www/js/routes.js
@@ -2,52 +2,80 @@ angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-    
-  
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-      .state('menu.resultados', {
-    url: '/resultados',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/resultados.html',
-        controller: 'resultadosCtrl'
-      }
-    }
-  })
+        .state('menu.enviarMuestra', {
+        url: '/upload',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/enviarMuestra.html',
+                controller: 'enviarMuestraCtrl'
+            }
+        }
+    })
 
-  .state('menu.detalleDeResultado', {
-    url: '/detalleDeResultado',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/detalleDeResultado.html',
-        controller: 'detalleDeResultadoCtrl'
-      }
-    }
-  })
+    .state('menu.home', {
+        url: '/home',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/home.html',
+                controller: 'homeCtrl'
+            }
+        }
+    })
 
-  .state('menu', {
-    url: '/side-menu21',
-    templateUrl: 'templates/menu.html',
-    controller: 'menuCtrl'
-  })
+    .state('menu', {
+        url: '/side-menu21',
+        templateUrl: 'templates/menu.html',
+        controller: 'menuCtrl'
+    })
 
-  .state('menu.perfil', {
-    url: '/perfil',
-    views: {
-      'side-menu21': {
-        templateUrl: 'templates/perfil.html',
-        controller: 'perfilCtrl'
-      }
-    }
-  })
 
-$urlRouterProvider.otherwise('/side-menu21/resultados')
+    .state('menu.resultados', {
+        url: '/resultados',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/resultados.html',
+                controller: 'resultsCtrl'
+            }
+        }
+    })
 
-  
+    .state('menu.detalleDeResultado', {
+        url: '/detalleDeResultado',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/detalleDeResultado.html',
+                controller: 'detalleDeResultadoCtrl'
+            }
+        }
+    })
+
+
+
+    .state('menu.perfil', {
+        url: '/perfil',
+        views: {
+            'side-menu21': {
+                templateUrl: 'templates/perfil.html',
+                controller: 'perfilCtrl'
+            }
+        }
+    })
+
+
+
+    $urlRouterProvider.otherwise('/side-menu21/home')
+
+
+
+    // if none of the above states are matched, use this as the fallback
+    //$urlRouterProvider.otherwise('/app');
+
+
 
 });
